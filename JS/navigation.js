b447e170c46240ad498837f464ca114b99dbc980
Phemelo Tsogang <2543064@students.wits.ac.za>
2026-06-05 03:09:31 +0200

    Nav and Home Page Implemented

---
+++ JS/navigation.js
@@ -0,0 +1,10 @@
+const navBar = document.querySelector(".nav-bar");
+
+navBar.innerHTML = 
+`
+    <a href="index.html" class="nav-link" id="home-btn" >Home</a>
+    <a href="guides.html" class="nav-link" "guides-btn">Beginner Guides</a>
+    <a href="recommendations.html" class="nav-link" "recomm-btn">Recommended Comics</a>
+    <a href="characters.html" class="nav-link" "char-btn">Characters</a>
+    <a href="glossary.html" class="nav-link" "glossary-btn">Glossary</a>
+`;
