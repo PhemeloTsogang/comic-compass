ab17c6506d413a06e33ebc4709ab7f765a6440c4
Phemelo Tsogang <2543064@students.wits.ac.za>
2026-06-08 01:57:23 +0200

    All 5 pages visually designed with 2 features added

--- JS/navigation.js
+++ JS/navigation.js
@@ -3,8 +3,8 @@ const navBar = document.querySelector(".nav-bar");
 navBar.innerHTML = 
 `
     <a href="index.html" class="nav-link" id="home-btn" >Home</a>
+    <a href="characters.html" class="nav-link" "char-btn">Characters</a>
     <a href="guides.html" class="nav-link" "guides-btn">Beginner Guides</a>
     <a href="recommendations.html" class="nav-link" "recomm-btn">Recommended Comics</a>
-    <a href="characters.html" class="nav-link" "char-btn">Characters</a>
     <a href="glossary.html" class="nav-link" "glossary-btn">Glossary</a>
 `;
